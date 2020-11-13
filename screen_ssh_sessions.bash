d53d3a2e3c79f5dc27b4fd19d9d707d5ac49af02
MichaIng <micha@dietpi.com>
2020-11-13 23:48:38 +0100

    Add script to install screen and use it for all SSH sessions

---
+++ screen_ssh_sessions.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Install and setup GNU Screen for SSH sessions
+# Created by MichaIng / micha@dietpi.com / dietpi.com
+{
+# Install Screen
+apt-get -q update
+apt-get -qq --no-install-recommends screen
+
+# Disable startup message
+sed -i '/^[[:blank:]#]*startup_message/c\#startup_message off' /etc/screenrc
+
+# Disable hardstatus line
+sed -i 's/^[[:blank:]]*hardstatus[[:blank:]]/#hardstatus /' /etc/screenrc
+sed -i 's/^[[:blank:]]*termcapinfo xterm\*|rxvt\*|kterm\*|Eterm\*/#termcapinfo xterm*|rxvt*|kterm*|Eterm*/' /etc/screenrc
+sed -Ei '/^#hardstatus[[:blank:]]+(on|off)/c\hardstatus off' /etc/screenrc
+
+# Enable scollback buffer
+sed -i '/^[[:blank:]#]*termcapinfo xterm|xterms|xs|rxvt ti@:te@$/c\termcapinfo xterm|xterms|xs|rxvt ti@:te@' /etc/screenrc
+
+# Additions
+cat << '_EOF_' >> /etc/screenrc
+# ------------------------------------------------------------------------------
+# MICHAS ADDITIONS
+# ------------------------------------------------------------------------------
+
+# Start screen as login shell to read /etc/profile[.d/]
+shell -/bin/bash
+
+# Remove whiptail/pager content after closing
+altscreen on
+
+# Add information status line to screen windows
+caption always "%{Gk}%-w%{kG}%n %t%{-}%+w"
+_EOF_
+
+# Automatically start screen on SSH sessions
+cat << '_EOF_' > /etc/profile.d/00-micha.sh
+# Autostart screen and auto logout
+[ "$TERM" == 'screen' ] || exec screen -U -S sshscreen -d -R
+_EOF_
+}
