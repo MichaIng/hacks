e714aa6886ae8d6435a746c6d3bd5a7026a5c9ed
MichaIng <micha@dietpi.com>
2020-11-13 22:35:11 +0100

    Add script to remove diversions for files which do not exist

---
+++ clean_dpkg_diversions.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Remove dpkg diversions for files which do not exist
+# Created by MichaIng / micha@dietpi.com / dietpi.com
+{
+i=0
+while read -r line
+do
+	(( $i == 3 )) && i=1 || ((i++))
+	(( $i == 1 )) || continue
+	[[ -e $line ]] || dpkg-divert --remove --no-rename "$line"
+
+done < /var/lib/dpkg/diversions
+}
