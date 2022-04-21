b1121a923376482c267601009ac428dd9eb533c3
MichaIng <micha@dietpi.com>
2022-04-21 15:26:02 +0200

    Remove support for pre-Bullseye ECDSA host key
    and increase APT verbosity.

--- dropbear_systemd.sh
+++ dropbear_systemd.sh
@@ -3,31 +3,29 @@
 # Created by MichaIng / micha@dietpi.com / dietpi.com
 {
 # Remove obsolete SysV and upstart files
-dpkg-query -s dropbear-run > /dev/null 2>&1 && { apt-get -qq purge dropbear-run || exit 1; }
-dpkg-query -s dropbear > /dev/null 2>&1 && { apt-get -qq purge dropbear || exit 1; }
+dpkg-query -s dropbear-run > /dev/null 2>&1 && { apt-get -y purge dropbear-run || exit 1; }
+dpkg-query -s dropbear > /dev/null 2>&1 && { apt-get -y purge dropbear || exit 1; }
 
 # Assure up-to-date Dropbear binary is installed
-apt-get -q update || exit 1
-apt-get -qq install --no-install-recommends dropbear-bin || exit 1
+apt-get update || exit 1
+apt-get -y --no-install-recommends install dropbear-bin || exit 1
 
 # Remove obsolete files
-[ -d '/etc/dropbear' ] && { rm -Rv /etc/dropbear || exit 1; }
-[ -f '/etc/default/dropbear' ] && { rm -v /etc/default/dropbear || exit 1; }
+rm -Rfv /etc/dropbear /etc/default/dropbear || exit 1
 
 # Create Ed25519 host key
-mkdir -p /etc/dropbear || exit 1
-grep -q bullseye /etc/os-release && type='ed25519' size= || type='ecdsa' size=521
-dropbearkey -t $type -f /etc/dropbear/dropbear_${type}_host_key ${size:+-s $size} || exit 1
+mkdir /etc/dropbear || exit 1
+dropbearkey -t ed25519 -f /etc/dropbear/dropbear_ed25519_host_key || exit 1
 
 # Create systemd unit
-cat << _EOF_ > /etc/systemd/system/dropbear.service || exit 1
+cat << '_EOF_' > /etc/systemd/system/dropbear.service || exit 1
 [Unit]
 Description=Dropbear
 Wants=network-online.target
 After=network-online.target
 
 [Service]
-ExecStart=/usr/sbin/dropbear -EFsr '/etc/dropbear/dropbear_${type}_host_key' -P ''
+ExecStart=/usr/sbin/dropbear -EFsr '/etc/dropbear/dropbear_ed25519_host_key' -P ''
 KillMode=process
 
 [Install]
