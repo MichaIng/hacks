1833825f02ca4caaa3d59461c816ef0d638973f5
MichaIng <micha@dietpi.com>
2024-06-13 22:54:29 +0200

    Use systemd-binfmt instead of binfmt-support

--- boot_img.bash
+++ boot_img.bash
@@ -18,8 +18,9 @@ G_EXEC test -f "$FP_IMG"
 disable_error=1 G_CHECK_VALIDINT "$FS_IMG" 0 || FS_IMG=2
 
 # Install QEMU emulation support when running from x86_64 host
-(( $G_HW_ARCH == 10 )) && emulation_packages=('qemu-user-static' 'binfmt-support') || emulation_packages=()
+(( $G_HW_ARCH == 10 )) && emulation_packages=('qemu-user-static') || emulation_packages=()
 G_AG_CHECK_INSTALL_PREREQ parted fdisk dosfstools dbus systemd-container "${emulation_packages[@]}"
+[[ ${emulation_packages[0]} ]] && G_EXEC systemctl restart systemd-binfmt
 LOOP_DEV=$(losetup -f)
 ROOT_DEV="${LOOP_DEV}p2"
 
