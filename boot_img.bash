ea02ae787974385df875e20d584446d768142cef
MichaIng <micha@dietpi.com>
2025-10-23 23:21:31 +0200

    further boot_img script updates and cleanup

--- boot_img.bash
+++ boot_img.bash
@@ -3,8 +3,8 @@
 . /boot/dietpi/func/dietpi-globals
 
 # Inputs:
-# - "-e": drop to shell for edit
-# - "-b": has boot partition: if set, assumes partition 1 to be boot partition, and partition 2 to be rootfs
+# - "-e", "--edit": drop to shell for edit
+# - "-b", "--boot-part": has boot partition: if set, assumes partition 1 to be boot partition, and partition 2 to be rootfs
 # - $1: img path
 # - $2: img size (GiB, optional, defaults to 2)
 EDIT=0
@@ -15,8 +15,8 @@ BOOT_PART=
 while (( $# ))
 do
 	case $1 in
-		'-e') EDIT=1;;
-		'-b') BOOT_PART=1 ROOT_PART=2;;
+		'-e'|'--edit') EDIT=1;;
+		'-b'|'--boot-part') BOOT_PART=1 ROOT_PART=2;;
 		*) [[ $FP_IMG ]] && FS_IMG=$1 || FP_IMG=$1;;
 	esac
 	shift
@@ -24,14 +24,20 @@ done
 G_EXEC test -f "$FP_IMG"
 disable_error=1 G_CHECK_VALIDINT "$FS_IMG" 0 || FS_IMG=2
 
-# Install QEMU emulation support when running from x86_64 host
+# Install dependencies
+# - Install FAT fsck if boot partition is used
+[[ $BOOT_PART ]] && dosfstools=('dosfstools') || dosfstools=()
+# - Install QEMU emulation support when running from x86_64 host
 (( $G_HW_ARCH == 10 )) && emulation_packages=('qemu-user-static') || emulation_packages=()
-G_AG_CHECK_INSTALL_PREREQ fdisk dbus systemd-container "${emulation_packages[@]}"
+G_AG_CHECK_INSTALL_PREREQ fdisk dbus systemd-container "${dosfstools[@]}" "${emulation_packages[@]}"
 [[ ${emulation_packages[0]} ]] && G_EXEC systemctl restart systemd-binfmt
+
+# Obtain device paths
 LOOP_DEV=$(losetup -f)
 ROOT_DEV="${LOOP_DEV}p$ROOT_PART"
-[[ $BOOT_PART ]] && BOOT_DEV="${LOOP_DEV}p$BOOT_PART" && G_AG_CHECK_INSTALL_PREREQ dosfstools
+[[ $BOOT_PART ]] && BOOT_DEV="${LOOP_DEV}p$BOOT_PART" 
 
+# Exit trap for cleanup
 G_EXIT_CUSTOM()
 {
 	# Revert workarounds
@@ -54,16 +60,11 @@ G_EXEC losetup -P "$LOOP_DEV" "$FP_IMG"
 G_EXEC_OUTPUT=1 G_EXEC e2fsck -fyD "$ROOT_DEV"
 [[ $BOOT_DEV ]] && G_EXEC_OUTPUT=1 G_EXEC fsck -y "$BOOT_DEV"
 
-# Raise image+partition+fs size if required, always run fsck
+# Raise image+partition+fs size if required
 if (( $FS_IMG && $(stat -c '%s' "$FP_IMG") < $FS_IMG*1024**3 ))
 then
 	G_EXEC truncate -s $(($FS_IMG*1024**3)) "$FP_IMG"
 	G_EXEC losetup -c "$LOOP_DEV"
-	if [[ $(blkid -s PTTYPE -o value -c /dev/null "$LOOP_DEV") == 'gpt' ]]
-	then
-		G_AG_CHECK_INSTALL_PREREQ gdisk
-		G_EXEC sgdisk -e "$LOOP_DEV"
-	fi
 	G_EXEC_OUTPUT=1 G_EXEC eval "sfdisk -N'${ROOT_DEV: -1}' '$LOOP_DEV' <<< ',+'"
 	G_EXEC_OUTPUT=1 G_EXEC resize2fs "$ROOT_DEV"
 fi
@@ -72,14 +73,19 @@ fi
 G_EXEC mkdir -p rootfs
 findmnt -M rootfs &> /dev/null && G_EXEC umount -R rootfs
 G_EXEC mount "$ROOT_DEV" rootfs
-[[ $BOOT_DEV ]] && G_EXEC mount "$BOOT_DEV" rootfs/boot
-
-# Drop to shell for edit
-(( $EDIT )) && bash
+if [[ $BOOT_DEV ]]
+then
+	if [[ -d '/boot/firmware' ]]
+	then
+		G_EXEC mount "$BOOT_DEV" rootfs/boot/firmware
+	else
+		G_EXEC mount "$BOOT_DEV" rootfs/boot
+	fi
+fi
 
 # Workarounds
 # - Mask Dropbear to prevent its package install from failing
-ln -s /dev/null rootfs/etc/systemd/system/dropbear.service
+G_EXEC ln -s /dev/null rootfs/etc/systemd/system/dropbear.service
 # - Prevent failing services and missing login prompt by disabling credentials in systemd units
 if [[ ${emulation_packages[0]} ]]
 then
@@ -91,11 +97,12 @@ then
 	done
 fi
 
-# dbus required for container spawn
-G_EXEC systemctl unmask dbus.socket dbus
-G_EXEC systemctl start dbus.socket dbus
-# Bind mounts required to allow container reading its own mount info
-systemd-nspawn -bD rootfs --bind="$LOOP_DEV" --bind="$ROOT_DEV" ${BOOT_DEV:+--bind="$BOOT_DEV"} --bind=/dev/disk
+# Drop to shell for edit
+(( $EDIT )) && bash
+
+# Boot
+# - Bind mounts required to allow container reading its own root device info: --bind="$LOOP_DEV" --bind="$ROOT_DEV" ${BOOT_DEV:+--bind="$BOOT_DEV"} --bind=/dev/disk
+systemd-nspawn -bD rootfs || exit 1
 
 exit 0
 }
